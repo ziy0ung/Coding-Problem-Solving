18fec5f0cc32af2c3787c9f58286dd9c89f96d37
ziy0ung <xoxojy@naver.com>
2021-09-06 23:55:40 +0900

    코드 수정

--- Baekjoon/b4344/app.js
+++ Baekjoon/b4344/app.js
@@ -12,15 +12,44 @@ const fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString();
 input = input.split('\n');
 console.log(input);
+const inputC = +input[0];
+const inputTestCase = [];
 
-const testCaseNum = +input[0];
-for (let i = 1; i <= testCaseNum; ++i) {
-  // const arr = input[i].split(' ');
-  // let newArr = [];
-  // for (let i = 0; i < arr.length; ++i) {
-  //   newArr.push(+arr[i]);
-  // }
-  const arr = input[i].split(' ').map((item) => Number(item));
-  console.log('arr : ', arr);
-  break;
+for (let i = 1; i <= inputC; ++i) {
+  const arr = input[i].split(' ');
+  let newArr = [];
+  for (let i = 0; i < arr.length; ++i) {
+    newArr.push(+arr[i]);
+  }
+  // * 위 4줄의 코드를 map을 이용해 한줄로 쓸 수 있다.
+  // const arr = input[i].split(' ').map((item) => Number(item));
+  const testCase = {
+    N: arr[0],
+    arr: newArr,
+  };
+
+  inputTestCase.push(testCase);
 }
+
+// * '입력을 어떻게 주면은 풀 수 있을까?' 생각해보기
+// * 미리 주석으로 구체적으로 적어본다.
+/*
+C = 5
+testCase = [
+  {
+    N: 5,
+    arr: [50, 50, 70, 80, 100]
+  }
+  {
+    N: 7,
+    arr: [50, 50, 70, 80, 100]
+  }
+  ...
+]
+*/
+function solution(C, testCase) {
+  console.log('C : ', C);
+  console.log('testCase : ', testCase);
+}
+
+solution(inputC, inputTestCase);
