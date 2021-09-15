827a8fcdc84b6dad64358b8b667d49608a0a81a0
ziy0ung <xoxopjy@gmail.com>
2021-09-15 21:30:49 +0900

    추가 0858_OX퀴즈

--- Baekjoon/4344_평균은 넘겠지/prac.js
+++ Baekjoon/4344_평균은 넘겠지/prac.js
@@ -1,5 +1,4 @@
 let input = require('fs').readFileSync('./input.txt').toString().split('\n');
-
 let num = input[0] * 1;
 
 for (let i = 1; i <= num; i++) {
@@ -8,5 +7,18 @@ for (let i = 1; i <= num; i++) {
     let count = 0;
 
     let avg = score.reduce((acc, v) => acc += v * 1, 0);
+
+    avg /= num2;
+
+    for (let j = 0; j < num2; j++) {
+        if (score[j] > avg) {
+            count++;
+        }
+    }
+
+    let result = ((count / num2) * 100).toFixed(3);
+    console.log(result + "%")
+    // console.log(avg)
+    
     
 }
---
+++ Baekjoon/8958_OX퀴즈/prac.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('./input.txt').toString().split('\n');
+
+let num = Number(input[0]);
+
+for (let i = 1; i <= num; i++) {
+  let count = 0;
+  let sum = 0;
+
+  for (let j = 0; j < input[i].length; j++) {
+    if (input[i][j] === "O") {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    sum += count;
+  }
+
+  console.log(sum);
+}
